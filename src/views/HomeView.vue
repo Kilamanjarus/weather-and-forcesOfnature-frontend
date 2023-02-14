e5af7db64adb68dcbd9b964554583e10538ebe25
max <max.lillie95@gmail.com>
2023-02-14 11:28:52 -0600

    Got api call working

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,21 +7,16 @@ export default {
       message: "Welcome to Vue.js!",
       lat: 42.8667,
       lon: 88.3334,
-      appid: process.env.VUE_APP_API_KEY,
     };
   },
   created: function () {
     this.getWeather();
   },
   methods: {
-    getGeoLocation: function () {
-      axios.get(`http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}`).then(response => {
-        console.log(response);
-      })
-    },
     getWeather: function () {
-      axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${this.appid}`).then(response => {
-        this.message = response.data.current.weather[0].description;
+      console.log(process.env.VUE_APP_WEATHER_API);
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=peshawar&appid=${process.env.VUE_APP_WEATHER_API}`).then(response => {
+        this.message = response.data;
         console.log(response.data);
       })
     }
