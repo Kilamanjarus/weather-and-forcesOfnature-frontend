53a3c4a77611b95d163b3dab9d5f1db6c5cf75b2
max <max.lillie95@gmail.com>
2023-02-14 17:06:22 -0600

    geolocation call is working

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,12 +7,19 @@ export default {
       message: "Welcome to Vue.js!",
       lat: 42.8667,
       lon: 88.3334,
+      address: { city_name: 'New York', state_code: 'NY', country_code: 'US' },
     };
   },
   created: function () {
     this.getWeather();
+    this.getGeocode();
   },
   methods: {
+    getGeocode: function () {
+      axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${this.address.city_name},${this.address.state_code},${this.address.country_code}&limit=3&appid=${process.env.VUE_APP_WEATHER_API}`).then(response => {
+        console.log(response.data);
+      })
+    },
     getWeather: function () {
       console.log(process.env.VUE_APP_WEATHER_API);
       axios.get(`https://api.openweathermap.org/data/2.5/weather?q=peshawar&appid=${process.env.VUE_APP_WEATHER_API}`).then(response => {
