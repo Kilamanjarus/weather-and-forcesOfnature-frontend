47d0546b1db6887eed2c1c5ccda8458b5165a3d1
max <max.lillie95@gmail.com>
2023-02-14 10:42:35 -0600

    trying to get openweather api to work, getting invalid api error but i don't know why

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,13 +5,24 @@ export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      lat: 42.8667,
+      lon: 88.3334,
+      appid: process.env.VUE_APP_API_KEY,
     };
   },
-  created: function () { },
+  created: function () {
+    this.getWeather();
+  },
   methods: {
+    getGeoLocation: function () {
+      axios.get(`http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}`).then(response => {
+        console.log(response);
+      })
+    },
     getWeather: function () {
-      axios.get("").then(response => {
-        console.log(response)
+      axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${this.appid}`).then(response => {
+        this.message = response.data.current.weather[0].description;
+        console.log(response.data);
       })
     }
   },
