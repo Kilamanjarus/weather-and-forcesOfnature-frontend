3d0098381a19def0fc04e1ded4c835d8dc1baea7
max <max.lillie95@gmail.com>
2023-02-15 10:21:01 -0600

    attempted to changed response into a blob before setting instead of changing the response into a blob, didn't work

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,10 +12,25 @@ export default {
       imageUrl: '',
     };
   },
+  mounted: function () {
+    const layer = 'temp_new';
+    const zoom = 5;
+    const x = 4;
+    const y = 4;
+    const url = `https://tile.openweathermap.org/map/${layer}/${zoom}/${x}/${y}?appid=${process.env.VUE_APP_WEATHER_API}`;
+    axios.get(url, { responseType: 'blob' })
+      .then(response => {
+        this.mapTileUrl = URL.createObjectURL(response.data);
+        console.log(this.mapTileUrl);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  },
   created: function () {
     this.getWeather();
     this.getGeocode();
-    this.getMap();
+    // this.getMap();
   },
   methods: {
     getGeocode: function () {
@@ -30,12 +45,12 @@ export default {
         this.message = response.data;
       })
     },
-    getMap: function () {
-      axios.get(`https://tile.openweathermap.org/map/temp_new/5/4/4?appid=${process.env.VUE_APP_WEATHER_API}`).then(response => {
-        this.imageUrl = URL.createObjectURL(new Blob([response.data]));
-        console.log(this.imageUrl);
-      })
-    }
+    // getMap: function () {
+    //   axios.get(`https://tile.openweathermap.org/map/temp_new/5/4/4?appid=${process.env.VUE_APP_WEATHER_API}`).then(response => {
+    //     this.imageUrl = URL.createObjectURL(new Blob([response.data]));
+    //     console.log(this.imageUrl);
+    //   })
+    // }
   },
 };
 </script>
@@ -43,9 +58,9 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <div class="map">
-      <img :src="this.imageUrl">
-    </div>
+  </div>
+  <div>
+    <img :src="mapTileUrl" alt="Map tile">
   </div>
 </template>
 
