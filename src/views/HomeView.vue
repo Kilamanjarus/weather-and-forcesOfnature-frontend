b1020ee2f24fece491ff8e6ded7960fe7d3a6174
max <max.lillie95@gmail.com>
2023-02-25 13:25:25 -0600

    added degrees to frontend, changed temperatures values to have all the correct symbols

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@ export default {
       message: "Welcome to Vue.js!",
       lat: 43,
       lon: -75,
-      address: { city_name: 'New York', state_code: 'NY', country_code: 'US' },
+      address: {},
       map: null,
       imageUrl: '',
       daily: [],
@@ -15,56 +15,60 @@ export default {
       minutely: [],
       current: [],
       icon: "",
+      current_description: "",
       temperatures: {}
     };
   },
   created: function () {
-    this.getWeather();
-    this.getGeocode();
-    this.getMap();
   },
   methods: {
     getGeocode: function () {
-      axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${this.address.city_name},${this.address.state_code},${this.address.country_code}&limit=3&appid=${process.env.VUE_APP_WEATHER_API}`).then(response => {
-        // console.log(response.data);
+      axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${this.address.city_name}&limit=3&appid=${process.env.VUE_APP_WEATHER_API}`).then(response => {
+        console.log(response.data);
+        this.lat = response.data[0].lat;
+        this.lon = response.data[0].lon;
+        console.log(response.data[0].lat)
+        this.getWeather(response.data[0].lat, response.data[0].lon);
       })
     },
-    getWeather: async function () {
-      axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${this.lat}&lon=${this.lon}&appid=${process.env.VUE_APP_WEATHER_API}`).then(response => {
+    getWeather: function (lat, lon) {
+      axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${process.env.VUE_APP_WEATHER_API}`).then(response => {
         // console.log(response.data);
         this.daily = response.data.daily;
         this.hourly = response.data.hourly;
         this.minutely = response.data.minutely;
         this.message = response.data.alerts;
         this.current = response.data.current;
+        this.current_description = this.toTitleCase(response.data.current.weather[0].description);
         this.icon = `http://openweathermap.org/img/wn/${response.data.current.weather[0].icon}@2x.png`;
-        this.temperatures.kelvin = response.data.current.temp;
-        this.temperatures.fahrenheit = (response.data.current.temp - 273.15) * 9 / 5 + 32;
-        this.temperatures.celsius = response.data.current.temp - 273.15;
+        this.temperatures.kelvin = Math.round(response.data.current.temp) + "K";
+        this.temperatures.fahrenheit = Math.round((response.data.current.temp - 273.15) * 9 / 5 + 32) + "\u00B0 F";
+        this.temperatures.celsius = Math.round(response.data.current.temp - 273.15) + "\u00B0 C";
         console.log(this.temperatures);
       })
     },
     getMap: function () {
-      axios.get(`https://tile.openweathermap.org/map/temp_new/5/4/4?appid=${process.env.VUE_APP_WEATHER_API}`).then(response => {
+      axios.get(`https://tile.openweathermap.org/map/temp_new/3/1/1?appid=${process.env.VUE_APP_WEATHER_API}`).then(response => {
         this.imageUrl = `https://tile.openweathermap.org/map/temp_new/5/4/4?appid=${process.env.VUE_APP_WEATHER_API}`;
         console.log(this.imageUrl);
       })
     },
+
   },
 };
 </script>
 
 <template>
   <div class="home">
+    <h1>{{ this.temperatures.fahrenheit }}</h1>
     <!-- <h1>{{ message }}</h1>
     <h1>{{ this.current }}</h1> -->
-    <h1>{{ this.current.weather[0].description.toUpperCase() }}</h1>
-    <div id="icon">
-      <img v-bind:src="this.icon">
-    </div>
-    <div id="temp_map">
+    <h1></h1>
+    <h1>{{ this.current_description }}</h1>
+    <img v-bind:src="this.icon">
+    <!-- <div id="temp_map">
       <img :src="this.imageUrl" alt="Map tile">
-    </div>
+    </div> -->
   </div>
 </template>
 
